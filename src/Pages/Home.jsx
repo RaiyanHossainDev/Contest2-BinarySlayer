94ba7e738899d778dbd9913f6b2b089f309f1588
zakinghaniweb <zakinghani1@gmail.com>
2024-12-30 15:00:36 +0600

    product commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from '../Components/Banner/Banner'
 import Category from '../Components/Category/Category'
 import './Home.css'
+import Product from '../Components/Product/Product'
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Category />
+      <Product />
     </div>
   )
 }
